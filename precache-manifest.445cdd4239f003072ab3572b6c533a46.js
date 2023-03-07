7e122844c1a8e2d0eb371c8c726a9b49b29c3a39
jayachand2001 <jayachand2001@gmail.com>
2023-03-07 12:52:44 +0530

    Updates

---
+++ precache-manifest.445cdd4239f003072ab3572b6c533a46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11ee659c40011af2fad",
+    "url": "/static/css/main.8a884b8d.chunk.css"
+  },
+  {
+    "revision": "c11ee659c40011af2fad",
+    "url": "/static/js/main.c11ee659.chunk.js"
+  },
+  {
+    "revision": "70b6787cc18b251c1ea1",
+    "url": "/static/js/1.70b6787c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7132685e291c1c237f7e7cee33137c98",
+    "url": "/static/media/dumbo.7132685e.png"
+  },
+  {
+    "revision": "71e4e7c8035737c7051c4b7436df78b6",
+    "url": "/static/media/helka.71e4e7c8.png"
+  },
+  {
+    "revision": "c9a67e744264d6b417aee0ce15eb5a19",
+    "url": "/static/media/Female sketch to face.c9a67e74.png"
+  },
+  {
+    "revision": "fc195ffcd6e7451c13862388a127adee",
+    "url": "/static/media/profilepic.fc195ffc.jpg"
+  },
+  {
+    "revision": "dccd8e2544e769b53ae3f0fe909b3e8f",
+    "url": "/static/media/backgroundimage.dccd8e25.jpg"
+  },
+  {
+    "revision": "bb57e8597c83ed3537b28b4bb5c31fef",
+    "url": "/index.html"
+  }
+];
